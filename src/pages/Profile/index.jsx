d0b75669c9dda954baf2377de024150438f9d997
Apolo Moraes <vlogsapolo@gmail.com>
2023-03-07 08:40:33 -0300

    carregando informações do usuário

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,11 +1,20 @@
+import { useState } from 'react';
 import { Container, Form, Avatar } from "./styles"
 import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi'
 import { Input } from "../../components/Input"
 import { Button } from "../../components/Button"
 import { Link } from 'react-router-dom'
+import { useAuth } from '../../hooks/auth';
 
 
 export function Profile() {
+  const { user } = useAuth();
+
+  const [name, setName] = useState(user.name);
+  const [email, setEmail] = useState(user.email);
+  const [passwordOld, setPasswordOld] = useState();
+  const [passwordNew, setPasswordNew] = useState();
+
   return (
     <Container>
       <header>
@@ -28,10 +37,32 @@ export function Profile() {
           </label>
         </Avatar>
 
-        <Input placeholder="Nome" type="text" icon={FiUser} />
-        <Input placeholder="E-mail" type="email" icon={FiMail} />
-        <Input placeholder="Senha atual" type="password" icon={FiLock} />
-        <Input placeholder="Nova senha" type="password" icon={FiLock} />
+        <Input
+          placeholder="Nome"
+          type="text"
+          icon={FiUser}
+          value={name}
+          onChange={event => setName(event.target.value)}
+        />
+        <Input
+          placeholder="E-mail"
+          type="email"
+          icon={FiMail}
+          value={email}
+          onChange={event => setEmail(event.target.value)}
+        />
+        <Input
+          placeholder="Senha atual"
+          type="password"
+          icon={FiLock}
+          onChange={event => setPasswordOld(event.target.value)}
+        />
+        <Input
+          placeholder="Nova senha"
+          type="password"
+          icon={FiLock}
+          onChange={event => setPasswordNew(event.target.value)}
+        />
 
 
         <Button title="Salvar" />
