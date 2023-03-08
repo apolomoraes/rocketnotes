61204642ced5eac4c65e9f6ae004f5eb59bd7a64
Apolo Moraes <vlogsapolo@gmail.com>
2023-03-08 11:42:29 -0300

    adicionando link na nota

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Header } from '../../components/Header'
 import { Input } from '../../components/Input'
 import { Textarea } from '../../components/Textarea'
@@ -5,11 +6,18 @@ import { NoteItem } from '../../components/NoteItem'
 import { Section } from '../../components/Section'
 import { Button } from '../../components/Button'
 import { Link } from 'react-router-dom'
-
-
 import { Container, Form } from './styles'
 
+
 export function New() {
+  const [links, setLinks] = useState([]);
+  const [newLink, setNewLink] = useState("");
+
+  function handleAddLink() {
+    setLinks(prevState => [...prevState, newLink])
+    setNewLink("");
+  }
+
   return (
     <Container>
       <Header />
@@ -25,8 +33,24 @@ export function New() {
           <Textarea placeholder="Observações" />
 
           <Section title="Links úteis">
-            <NoteItem value="www.google.com" />
-            <NoteItem isNew placeholder="Novo link" />
+            {
+              links.map((link, index) => (
+                <NoteItem
+                  key={String(index)}
+                  value={link}
+                  onClick={() => { }}
+                />
+              ))
+
+            }
+
+            <NoteItem
+              isNew
+              placeholder="Novo link"
+              value={newLink}
+              onChange={event => setNewLink(event.target.value)}
+              onClick={handleAddLink}
+            />
           </Section>
 
           <Section title="Tags">
