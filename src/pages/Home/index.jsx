2c149f3dd4b2d7dcc8f2a281091dcf6c5ffd10fb
Apolo Moraes <vlogsapolo@gmail.com>
2023-03-09 13:47:46 -0300

    filtrar tag selecionada

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -11,8 +11,18 @@ import { api } from '../../services/api'
 
 export function Home() {
   const [tags, setTags] = useState([]);
+  const [tagsSelected, setTagsSelected] = useState([]);
 
+  function handleTagSelected(tagName) {
+    const alreadySelected = tagsSelected.includes(tagName);
 
+    if (alreadySelected) {
+      const filteredTags = tagsSelected.filter(tag => tag !== tagName);
+      setTagsSelected(filteredTags);
+    } else {
+      setTagsSelected(prevState => [...prevState, tagName]);
+    }
+  }
 
   useEffect(() => {
     async function fetchTags() {
@@ -35,7 +45,8 @@ export function Home() {
         <li>
           <ButtonText
             title="Todos"
-            isActive
+            onClick={() => handleTagSelected("all")}
+            isActive={tagsSelected.length === 0}
           />
         </li>
         {
@@ -43,6 +54,8 @@ export function Home() {
             <li key={String(tag.id)} >
               <ButtonText
                 title={tag.name}
+                onClick={() => handleTagSelected(tag.name)}
+                isActive={tagsSelected.includes(tag.name)}
               />
             </li>
           ))
