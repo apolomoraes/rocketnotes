be1df26ed3a6bb4867fa36a1b410af003ff41a48
Apolo Moraes <vlogsapolo@gmail.com>
2023-03-09 12:08:53 -0300

    carregando tags

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,5 @@
+
+import { useState, useEffect } from 'react';
 import { FiPlus, FiSearch } from 'react-icons/fi';
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles'
 import { Header } from '../../components/Header'
@@ -5,8 +7,22 @@ import { Input } from '../../components/Input'
 import { Note } from '../../components/Note'
 import { Section } from '../../components/Section'
 import { ButtonText } from '../../components/ButtonText'
+import { api } from '../../services/api'
 
 export function Home() {
+  const [tags, setTags] = useState([]);
+
+
+
+  useEffect(() => {
+    async function fetchTags() {
+      const response = await api.get("/tags");
+      setTags(response.data);
+    }
+
+    fetchTags();
+  }, [])
+
   return (
     <Container>
       <Brand>
@@ -17,14 +33,21 @@ export function Home() {
 
       <Menu>
         <li>
-          <ButtonText title="Todos" isActive />
+          <ButtonText
+            title="Todos"
+            isActive
+          />
         </li>
-        <li>
-          <ButtonText title="React" />
-        </li>
-        <li>
-          <ButtonText title="Node" />
-        </li>
+        {
+          tags && tags.map(tag => (
+            <li key={String(tag.id)} >
+              <ButtonText
+                title={tag.name}
+              />
+            </li>
+          ))
+        }
+
       </Menu>
 
       <Search>
