d0f724301e78b0437cdda854eeca52cd9f4a5ed1
Apolo Moraes <vlogsapolo@gmail.com>
2023-03-10 16:53:13 -0300

    redirecionando o botão de sair/desligar

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,9 +3,17 @@ import { Container, Profile, Logout } from "./styles";
 import { useAuth } from '../../hooks/auth';
 import { api } from "../../services/api"
 import avatarPlaceholder from '../../assets/avatar_placeholder.svg'
+import { useNavigate } from 'react-router-dom';
 
 export function Header() {
   const { signOut, user } = useAuth();
+
+  const navigation = useNavigate();
+
+  function handleSignOut() {
+    navigation("/");
+    signOut();
+  }
 
   const avatarURL = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
 
@@ -24,7 +32,7 @@ export function Header() {
         </div>
       </Profile>
 
-      <Logout onClick={signOut} >
+      <Logout onClick={handleSignOut} >
         <RiShutDownLine />
       </Logout>
 
