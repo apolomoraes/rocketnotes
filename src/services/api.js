f88bbccc79d0a3494d63f8d6527378f1ca0a8871
Apolo Moraes <vlogsapolo@gmail.com>
2023-03-29 16:38:30 -0300

    add url back-end

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 // essa variável contém o baseUrl para reaproveitar a url, que é o endereço do servidor.
 export const api = axios.create({
-  baseURL: "http://localhost:3333"
+  baseURL: "https://rocketnotes-api-ro0i.onrender.com"
 });
 
